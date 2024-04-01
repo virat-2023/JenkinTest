879fc4c220634c18e643d3952ebd352a71c494e7
virat-2023 <150992909+virat-2023@users.noreply.github.com>
2024-04-01 16:12:39 +0530

    Add files via upload

---
+++ JenkinTest/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JenkinTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Successsss";
+        }
+    }
+}
